d6119b78e272b69dcc83f92634cbe657221ee520
Sean Cheng <sainteye.x.deatheye@gmail.com>
2023-06-20 17:11:45 +0800

    update module_loading.py

--- django/utils/module_loading.py
+++ django/utils/module_loading.py
@@ -47,7 +47,8 @@ def module_has_submodule(package, module_name):
         # a package (module with a __path__), so if it's not, then bail here.
         return False
     for finder in sys.meta_path:
-        if finder.find_module(name, package_path):
+        find_module = finder.find_spec if hasattr(finder, 'find_spec') else finder.find_module
+        if find_module(name, package_path):
             return True
     for entry in package_path:
         try:
